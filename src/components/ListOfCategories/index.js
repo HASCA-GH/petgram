87b637df0527fe5a5d6f7cf72e071509451b2575
Humberto Asca <hasca@ciplima.org.pe>
2023-01-29 13:30:58 -0500

    Adding loaders/animation to the loading process of categories.

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,21 +1,90 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles';
-import data from '../../db.json';
+// import data from '../../db.json';
+
+// Custom Hook useCategoriesDate. Ojo un Hook siempre tiene que devolver algo por ende utilizamos el return:
+const useCategoriesData = () => {
+  const [categories,setCategories] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(function () {
+    setLoading(true)
+    fetch('https://dgotv2-server.vercel.app/categories')
+    .then(res => res.json())
+    .then(response => {
+      setCategories(response)
+      setLoading(false)
+    })
+  }, [])
+
+  return {categories, loading}
+
+}
 
 export const ListOfCategories = () => {
-  return (
-    <List>
-        {data.categories.map(category => 
+  const {categories, loading} = useCategoriesData()
+  const [showFixed,setShowFixed] = useState([false])
+
+
+  useEffect(() => {
+    const  onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+    
+    document.addEventListener('scroll', onScroll)
+
+    return ()=> document.removeEventListener('scroll', onScroll)
+    
+  }, [showFixed])
+  
+
+  const renderList = (fixed) => (
+    // <List fixed={fixed}>
+    //   {categories.map(category => 
+    //       <Item key={category.id}>
+    //           <Category 
+    //               cover={category.cover}
+    //               path={category.path}
+    //               emoji={category.emoji}
+    //           />
+    //       </Item>)
+    //   }
+    // </List>
+    <List fixed={fixed}>
+      {
+        loading 
+          // ? <Item key={loading}>Cargando ...<Category/></Item>
+          // ? <Item key={loading}><div className='xxx' style={{backgroundColor: "red"}}>Cargando ...</div></Item>
+          ? <Item key={loading}>
+              <div class="lds-hourglass"></div>
+              <div class="lds-hourglass"></div>
+              <div class="lds-hourglass"></div>
+              <div class="lds-hourglass"></div>
+            </Item>
+          : categories.map(category => 
             <Item key={category.id}>
-                <Category 
-                    cover={category.cover}
-                    path={category.path}
-                    emoji={category.emoji}
-                />
+              <Category 
+                  cover={category.cover}
+                  path={category.path}
+                  emoji={category.emoji}
+              />
             </Item>)
-        }
+      }
     </List>
+  )
+
+  // if (loading) {
+  //   return 'Cargando .....'
+  // }
+  return (
+    <>
+      {/* {loading ? 'Cargando ... ' : ''} */}
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
 
--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -1,11 +1,72 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 
 export const  List = styled.ul`
     display: flex;
     overflow: scroll;
     width: 100%;
+    ${props => props.fixed && css`
+        {
+            background: #fff;
+            border-radius: 60px;
+            box-shadow: 0 0 20px rgba(0,0,0,0.3);
+            left: 0;
+            margin: 0 auto;
+            max-width: 400px;
+            padding: 5px;
+            position: fixed;
+            right: 0;
+            top: -20px;
+            transform: scale(.5);
+            z-index: 1;
+        }
+    `}
+    /* &.fixed {
+        background: #fff;
+        border-radius: 60px;
+        box-shadow: 0 0 20px rgba(0,0,0,0.3);
+        left: 0;
+        margin: 0 auto;
+        max-width: 400px;
+        padding: 5px;
+        position: fixed;
+        right: 0;
+        top: -20px;
+        transform: scale(.5);
+        z-index: 1;
+    } */
 `
-
 export const  Item = styled.li`
     padding: 0px 8px;
+    & .lds-hourglass {
+        display: inline-block;
+        position: relative;
+        width: 80px;
+        height: 80px;
+    }
+    & .lds-hourglass:after {
+        content: " ";
+        display: block;
+        border-radius: 50%;
+        width: 0;
+        height: 0;
+        margin: 8px;
+        box-sizing: border-box;
+        border: 32px solid #dfc;
+        border-color: #dfc transparent #dfc transparent;
+        animation: lds-hourglass 1.2s infinite;
+    }
+    @keyframes lds-hourglass {
+    0% {
+        transform: rotate(0);
+        animation-timing-function: cubic-bezier(0.55, 0.055, 0.675, 0.19);
+    }
+    50% {
+        transform: rotate(900deg);
+        animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
+    }
+    100% {
+        transform: rotate(1800deg);
+    }
+    }
+
 `
